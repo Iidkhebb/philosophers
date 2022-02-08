0e46dafdc8d16100d677c7ebcf3697b288a14d97
Ilyasse Idkhebbach <iidkhebb@e1r4p15.1337.ma>
2022-02-08 20:20:24 +0100

    added lstnew strlen

--- philo.h
+++ philo.h
@@ -5,8 +5,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/time.h>
+#include <pthread.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 
-
-int ft_timestamp_in_ms();
+int		ft_timestamp_in_ms();
+t_list	*ft_lstnew(void *content);
 #endif
